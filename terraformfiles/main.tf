5e2d2676622290ce05687679373c7f768b2c9c8e
SnehalDatir <snehudatir12@gmail.com>
2026-06-13 12:21:23 +0000

    tf fmt check resolved

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,21 +1,21 @@
 
 resource "azurerm_resource_group" "rg" {
-name = "snehal-rg-cicd-ntms-workshop"
-location = "East US"
+  name     = "snehal-rg-cicd-ntms-workshop"
+  location = "East US"
 }
 
 
 resource "azurerm_virtual_network" "vnet" {
-name = "snehal-vnet-cicd-workshop"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-address_space = ["10.0.0.0/16"]
+  name                = "snehal-vnet-cicd-workshop"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
 
 resource "azurerm_subnet" "subnet" {
-name = "web-subnet"
-virtual_network_name = azurerm_virtual_network.vnet.name
-resource_group_name = azurerm_resource_group.rg.name
+  name                 = "web-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
