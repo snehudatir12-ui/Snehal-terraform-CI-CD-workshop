570470d68851917a52f4c7a1fa60b90b052b2712
SnehalDatir <snehudatir12@gmail.com>
2026-06-13 12:48:18 +0000

    subnet prefix added1

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -17,6 +17,6 @@ resource "azurerm_subnet" "subnet" {
   name                 = "web-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefixes = [ 10.0.0.0/24 ]
+  address_prefixes = [ "10.0.0.0/24" ]
 }
 
